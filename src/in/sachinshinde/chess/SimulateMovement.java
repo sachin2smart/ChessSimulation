15b9f1cf0f884a78721b1742c190d33a4b2cec5d
Sachin <sachin2smart@gmail.com>
2020-09-23 23:59:55 +0530

    Added main program for control of execution

---
+++ src/in/sachinshinde/chess/SimulateMovement.java
@@ -0,0 +1,35 @@
+package in.sachinshinde.chess;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+
+import in.sachinshinde.chess.simulation.SimulateMoves;
+import in.sachinshinde.chess.validation.Validate;
+
+public class SimulateMovement {
+
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Specify the Piece:");
+		String piece = br.readLine();
+		
+		if(!Validate.isVlaidPiece(piece))
+			return;
+		
+		System.out.println("Specify the Position:");
+		String position = br.readLine();
+		
+		if(!Validate.isVlaidPosition(position))
+			return;
+		
+		List<String> possibleMoves = SimulateMoves.getSimulatedMoves(piece, position);
+		System.out.println("Possible Moves are:");
+		possibleMoves.stream()
+			.forEach(System.out::println);
+	}
+
+	
+}
