a61cd161eaa005338ccc69465062c52112dd4349
Sachin <sachin2smart@gmail.com>
2020-09-24 01:31:52 +0530

    Added rules for UP movement

--- src/in/sachinshinde/chess/Rules/URules.java
+++ src/in/sachinshinde/chess/Rules/URules.java
@@ -8,12 +8,23 @@ public class URules {
 	
 	// Up Unlimited moves
 	public List<String> getUUmoves(String position){
-		List<String> uuMoves = new ArrayList<String>();
+		List<String> uMoves = new ArrayList<String>();
 		String member = String.valueOf(position.charAt(0));
 		int pointer = Integer.parseInt(String.valueOf(position.charAt(1)));
 		for(pointer = pointer+1; pointer<=8; pointer++) {
-			uuMoves.add(member + String.valueOf(pointer));
+			uMoves.add(member + String.valueOf(pointer));
 		}
-		return uuMoves;
+		return uMoves;
+	}
+	
+	// Up by 1 
+	public List<String> getU1moves(String position){
+		List<String> uMoves = new ArrayList<String>();
+		String member = String.valueOf(position.charAt(0));
+		int pointer = Integer.parseInt(String.valueOf(position.charAt(1)));
+		pointer = pointer+1;
+		if(pointer<=8)
+			uMoves.add(member + String.valueOf(pointer));
+		return uMoves;
 	}
 }
