05c576109f20b24851d82b446904a02e6807f412
Sachin <sachin2smart@gmail.com>
2020-09-24 00:00:45 +0530

    Started working on simulating the possible moves

---
+++ src/in/sachinshinde/chess/simulation/SimulateMoves.java
@@ -0,0 +1,13 @@
+package in.sachinshinde.chess.simulation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SimulateMoves {
+	
+	public static List<String> getSimulatedMoves(String piece, String position){
+		List<String> moves = new ArrayList<String>();
+		return moves;
+	}
+
+}
