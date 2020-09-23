faf8d023fab98da57c73e56f5109213a901641e5
Sachin <sachin2smart@gmail.com>
2020-09-23 23:47:09 +0530

    Added enum for Direction

---
+++ src/in/sachinshinde/chess/model/Direction.java
@@ -0,0 +1,23 @@
+package in.sachinshinde.chess.model;
+
+public enum Direction {
+	UU("Up", "Unlimited"),	//	Up unlimited 
+	DU("Down", "Unlimited"),	//	Down Unlimited	
+	LU("Left", "Unlimted"),	// 	Left Unlimited
+	RU("Right", "Unlimited"),	//	Right Unlimited
+	U1("Up", "One"),	//	Up by 1
+	D1("Down", "One"),	//	Down by 1
+	L1("Left", "One"),	//	Left by 1
+	R1("Right", "One"),	//	Right by 1
+	DG("Diagonal", "Any"),
+	DG1("Diagonal", "One"),
+	TH("Two", "Half");	//	Diagonal 
+	
+	private final String movement;
+	private final String projection;
+	
+	Direction(String movement, String projection){
+		this.movement = movement;
+		this.projection = projection;
+	}
+}
