8f0236b2aa0b4d6600c08679c290ca2d0620cf10
Sachin <sachin2smart@gmail.com>
2020-09-24 08:26:00 +0530

    Code cleanup in Direction Enum

--- src/in/sachinshinde/chess/model/Direction.java
+++ src/in/sachinshinde/chess/model/Direction.java
@@ -9,7 +9,7 @@ public enum Direction {
 	D1,	//	Down by 1
 	L1,	//	Left by 1
 	R1,	//	Right by 1
-	DG, //	Diagonal at any direction
-	DG1, // Diagonal at any direction by 1
+	DG,	//	Diagonal at any direction
+	DG1,	// Diagonal at any direction by 1
 	TH;	//	Two and half 
 }
