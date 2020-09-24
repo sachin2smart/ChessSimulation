d31149b050bd8aec8d8a1121c561c55ade860b25
Sachin <sachin2smart@gmail.com>
2020-09-24 08:24:52 +0530

    Code cleanup in Direction Enum

--- src/in/sachinshinde/chess/model/Direction.java
+++ src/in/sachinshinde/chess/model/Direction.java
@@ -1,23 +1,15 @@
 package in.sachinshinde.chess.model;
 
 public enum Direction {
-	UU("Up", "Unlimited"),	//	Up unlimited 
-	DU("Down", "Unlimited"),	//	Down Unlimited	
-	LU("Left", "Unlimted"),	// 	Left Unlimited
-	RU("Right", "Unlimited"),	//	Right Unlimited
-	U1("Up", "One"),	//	Up by 1
-	D1("Down", "One"),	//	Down by 1
-	L1("Left", "One"),	//	Left by 1
-	R1("Right", "One"),	//	Right by 1
-	DG("Diagonal", "Any"), // Diagonal at any direction
-	DG1("Diagonal", "One"), // Diagonal at any direction by 1
-	TH("Two", "Half");	//	Two and half 
-	
-	private final String movement;
-	private final String projection;
-	
-	Direction(String movement, String projection){
-		this.movement = movement;
-		this.projection = projection;
-	}
+	UU,	//	Up unlimited 
+	DU,	//	Down Unlimited	
+	LU,	// 	Left Unlimited
+	RU,	//	Right Unlimited
+	U1,	//	Up by 1
+	D1,	//	Down by 1
+	L1,	//	Left by 1
+	R1,	//	Right by 1
+	DG, //	Diagonal at any direction
+	DG1, // Diagonal at any direction by 1
+	TH;	//	Two and half 
 }
